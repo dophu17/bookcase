895f9b70103f149f4115e8b52ecac145d86eec6f
dophu17 <dophu17@gmail.com>
2025-07-23 15:04:07 +0700

    remove goocle cloud vision AI

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,7 +7,6 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 
 class BookController extends Controller
 {
